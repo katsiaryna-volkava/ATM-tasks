ebc90bd9dc5871f02ace365e2dbd04a5b634a800
Katsiaryna Volkava <katsiaryna_volkava@epam.com>
2019-03-04 14:40:55 +0300

    making changes in calculator (now using BufferedReader)

--- src/main/java/com/epam/atm/calculator/Calculator.java
+++ src/main/java/com/epam/atm/calculator/Calculator.java
@@ -1,38 +1,42 @@
 package com.epam.atm.calculator;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 
 public class Calculator {
-    public static void main(String[] args) {
-        Scanner Calculator = new Scanner(System.in);
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         double firstNumber, secondNumber, result;
-        String inputAction;
+        String operator;
 
 
         System.out.println("Input the first number: ");
-        firstNumber = Calculator.nextDouble();
+        firstNumber = Double.parseDouble(reader.readLine());
 
-        System.out.println("Input the ariphmetical sign: ");
-        inputAction = Calculator.next();
+        System.out.println("Input the operator: ");
+        operator = reader.readLine();
 
         System.out.println("Input the second number: ");
-        secondNumber = Calculator.nextDouble();
+        secondNumber = Double.parseDouble(reader.readLine());
 
 
-        if (inputAction.equals("+")) {
+
+        if (operator.equals("+")) {
             result = (firstNumber + secondNumber);
             System.out.println("The result is the following: " + result);
         }
-        if (inputAction.equals("-")) {
+        if (operator.equals("-")) {
             result = (firstNumber - secondNumber);
             System.out.println("The result is the following: " + result);
         }
-        if (inputAction.equals("*")) {
+        if (operator.equals("*")) {
             result = (firstNumber * secondNumber);
             System.out.println("The result is the following: " + result);
         }
-        if (inputAction.equals("/")) {
+        if (operator.equals("/")) {
             result = (firstNumber / secondNumber);
             System.out.println("The result is the following: " + result);
         }
