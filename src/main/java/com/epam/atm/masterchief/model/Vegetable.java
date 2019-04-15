228c5eb4ae043d91e4672c43e6a4b1a07edd7a16
Katsiaryna Volkava <katsiaryna_volkava@epam.com>
2019-04-15 10:04:16 +0300

    adding vegetables module

---
+++ src/main/java/com/epam/atm/masterchief/model/Vegetable.java
@@ -0,0 +1,97 @@
+package com.epam.atm.masterchief.model;
+
+public abstract class Vegetable {
+    private String name;
+    private int weight;
+    private int calories;
+    private int carbohydrate;
+    private int fiber;
+    private int sugar;
+    private int protein;
+    private int fat;
+
+
+    public Vegetable(String name, int weight, int calories, int carbohydrate, int fiber, int sugar, int protein, int fat) {
+        this.name = name;
+        this.weight = weight;
+        this.calories = calories;
+        this.carbohydrate = carbohydrate;
+        this.fiber = fiber;
+        this.sugar = sugar;
+        this.protein = protein;
+        this.fat = fat;
+    }
+
+    public Vegetable(String name, int weight, int calories) {
+        this.name = name;
+        this.weight = weight;
+        this.calories = calories;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public int getCalories() {
+        return calories;
+    }
+
+    public void setCalories(int calories) {
+        this.calories = calories;
+    }
+
+    public int getCarbohydrate() {
+        return carbohydrate;
+    }
+
+    public void setCarbohydrate(int carbohydrate) {
+        this.carbohydrate = carbohydrate;
+    }
+
+    public int getFiber() {
+        return fiber;
+    }
+
+    public void setFiber(int fiber) {
+        this.fiber = fiber;
+    }
+
+    public int getSugar() {
+        return sugar;
+    }
+
+    public void setSugar(int sugar) {
+        this.sugar = sugar;
+    }
+
+    public int getProtein() {
+        return protein;
+    }
+
+    public void setProtein(int protein) {
+        this.protein = protein;
+    }
+
+    public int getFat() {
+        return fat;
+    }
+
+    public void setFat(int fat) {
+        this.fat = fat;
+    }
+}
+
+
+}
